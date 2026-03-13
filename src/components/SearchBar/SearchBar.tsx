49355ea39514ff156e653dd98f8903a836bc60f2
Ihor Kuzmenko <nameless1306@gmail.com>
2026-03-13 17:07:26 +0200

    fix mistakes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,22 @@
 import css from "./SearchBar.module.css";
 import toast from "react-hot-toast";
-import { Formik, Form, Field } from "formik";
 
 interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleAction = async (formData: FormData) => {
+    const query = formData.get("query")?.toString().trim() ?? "";
+
+    if (!query) {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(query);
+  };
+
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -19,44 +29,19 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
           Powered by TMDB
         </a>
 
-        <Formik
-          initialValues={{ query: "" }}
-          validate={(values) => {
-            const errors: { query?: string } = {};
-            if (!values.query.trim()) {
-              errors.query = "Please enter your search query.";
-            }
-            return errors;
-          }}
-          onSubmit={(values, { resetForm }) => {
-            if (!values.query.trim()) {
-              toast.error("Please enter your search query.");
-              return;
-            }
-            onSubmit(values.query.trim());
-            resetForm();
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form className={css.form}>
-              <Field
-                className={css.input}
-                type="text"
-                name="query"
-                autoComplete="off"
-                placeholder="Search movies..."
-                autoFocus
-              />
-              <button className={css.button} type="submit">
-                Search
-              </button>
-
-              {errors.query && touched.query && (
-                <div className={css.error}>{errors.query}</div>
-              )}
-            </Form>
-          )}
-        </Formik>
+        <form className={css.form} action={handleAction}>
+          <input
+            className={css.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={css.button} type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </header>
   );
